89bf1916741cc949c267eddba0c845e7e323f726
Benoit SAVINEL <bsavinel@e2r3p12.clusters.42paris.fr>
2021-12-07 08:44:10 +0100

    lib + gnl non corrige

---
+++ Cercle_1/libft/ft_lstadd_back.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsavinel <bsavinel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/25 16:25:33 by bsavinel          #+#    #+#             */
+/*   Updated: 2021/11/29 09:52:47 by bsavinel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **alst, t_list *new)
+{
+	t_list	*tmp;
+
+	if (*alst == NULL)
+	{
+		*alst = new;
+		return ;
+	}
+	tmp = ft_lstlast(*alst);
+	tmp->next = new;
+}
---
+++ Cercle_1/libft/ft_lstlast.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsavinel <bsavinel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/25 16:25:51 by bsavinel          #+#    #+#             */
+/*   Updated: 2021/11/29 11:25:57 by bsavinel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*last;
+
+	last = lst;
+	if (!last)
+		return (lst);
+	while (last->next != NULL)
+		last = last->next;
+	return (last);
+}
