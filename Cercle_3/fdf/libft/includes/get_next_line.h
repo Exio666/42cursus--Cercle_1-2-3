fe2c84ac67c9f80215bc1119d7aaaa5f5b985539
Benoit SAVINEL <bsavinel@e2r3p12.clusters.42paris.fr>
2022-01-15 15:15:35 +0100

    lib mise a jour

---
+++ Cercle_3/fdf/libft/includes/get_next_line.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsavinel <bsavinel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/30 08:36:07 by bsavinel          #+#    #+#             */
+/*   Updated: 2022/01/14 15:58:56 by bsavinel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+#define BUFFER_SIZE 100
+
+/*
+ *  get_next_line.c
+ */
+
+char	*ft_joinstr(char *str, char *buf);
+int		ft_new_line(char *str);
+void	ft_replace(char *str);
+char	*get_next_line(int fd);
+char	*ft_line_return(char *str, char *tab);
+
+/*
+ *  get_next_line_utils.c
+ */
+
+void	ft_replace2(char *str, char *tmp, int i, int j);
+void	ft_joinstr2(char *str, char *buf, char *ret);
+int		ft_strlen(char *str);
+void	ft_bzero(void *s, size_t n);
+char	*ft_end(char *str);
+
+#endif
