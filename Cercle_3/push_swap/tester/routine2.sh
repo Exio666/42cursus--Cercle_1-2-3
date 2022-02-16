ef09dddb6fb6c0053a79c9aeb478f9220706f563
Benoit SAVINEL <bsavinel@e2r3p12.clusters.42paris.fr>
2022-02-16 11:08:28 +0100

    push swap: testeur mis a jour

---
+++ Cercle_3/push_swap/tester/routine2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+make
+clear
+echo "generateur du github de charlesjullien"
+while true
+do 
+    ARG=`generateur $1 -2147483648 2147483647`
+    #echo $ARG
+    #../push_swap $ARG
+    ../push_swap $ARG | wc -l
+    ../push_swap $ARG | ./checker_linux $ARG    
+    #sleep 0.4
+done
